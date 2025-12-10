8a3f9be5dcef7391d98f4f1598ed2acc7a73b90b
Shaun Doe <shaun@cofit.me>
2025-12-10 16:22:53 +0800

    adjust import method

---
+++ src/screens/shared/index.ts
@@ -0,0 +1,3 @@
+export { default as LoginPage } from './LoginPage';
+export { default as NotificationsScreen } from './NotificationsScreen';
+export { default as ProfileScreen } from './ProfileScreen';
