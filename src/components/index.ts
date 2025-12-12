2e270418c20a2de76588b290ed9af8eb0c518e15
Shaun Doe <shaun@cofit.me>
2025-12-12 16:59:57 +0800

    remove unuse code

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,7 +8,6 @@ export { default as MyAlert } from './common/MyAlert.tsx';
 export { default as Accordion } from './common/Accordion.tsx';
 export { default as CustomerForm } from './customerForm/index.tsx';
 export { default as BottomSheetModal } from './common/BottomSheetModal.tsx';
-export { default as DateRangePicker } from './common/DateRangePicker.tsx';
 export { default as LoginFooter } from './common/LoginFooter.tsx';
 export { default as AccountLoginForm } from './common/AccountLoginForm.tsx';
 export { default as PhoneLoginForm } from './common/PhoneLoginForm.tsx';
