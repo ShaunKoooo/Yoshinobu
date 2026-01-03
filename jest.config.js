b97bb8c45809fa75024227be622cf08c2194f853
Shaun Doe <shaun@cofit.me>
2026-01-03 14:10:21 +0800

    setting test environment

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|@react-navigation|@reduxjs/toolkit|immer)',
+  ],
 };
